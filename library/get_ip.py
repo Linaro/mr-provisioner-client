e37aaa21979202d5ca5b16420ae87d1be4a4f5db
Baptiste Gerondeau <bgerondeau@gmail.com>
2018-06-05 18:33:07 +0100

    Added client entrypoint to retrieve ip, mac and netmask info from MrP

--- library/get_ip.py
+++ library/get_ip.py
@@ -3,17 +3,19 @@
 
 import requests
 import json
-from common import ProvisionerError
+from library.common import ProvisionerError
+from library.common import get_machine_by_name
 from urllib.parse import urljoin
 from urllib.parse import quote
 
+
 class IPGetter(object):
-    def __init__(self, mrpurl, mrptoken, machine_id, interface_name =
-                 'eth1'):
+    def __init__(self, mrpurl, mrptoken, machine_name, interface_name='eth1'):
         self.mrp_url = mrpurl
         self.mrp_token = mrptoken
         self.interface = interface_name
-        self.machine_id = machine_id
+        self.machine_id = get_machine_by_name(
+            self.mrp_token, self.mrp_url, machine_name)['id']
         self.machine_ip = ''
 
     def get_interfaces(self):
@@ -22,10 +24,11 @@ class IPGetter(object):
                       "/api/v1/machine/{}/interface".format(self.machine_id))
         r = requests.get(url, headers=headers)
         if r.status_code != 200:
-            raise ProvisionerError('Error fetching {}, HTTP {} {}'.format(self.mrp_url, r.status_code,
-                                                         r.reason))
+            raise ProvisionerError('Error fetching {} {}, HTTP {} {}'.format(url, self.interface, r.status_code,
+                                                                             r.reason))
         if len(r.json()) == 0:
-            raise ProvisionerError('Error no machine with id "{}"'.format(self.machine_id))
+            raise ProvisionerError(
+                'Error no machine with id "{}"'.format(self.machine_id))
 
         return r.json()
 
@@ -38,5 +41,26 @@ class IPGetter(object):
 
         for i in interfaces:
             if str(i['identifier']) == self.interface:
-                    return i['lease_ipv4']
+                return i['lease_ipv4']
 
+    def get_mac(self):
+        try:
+            interfaces = self.get_interfaces()
+        except ProvisionerError as e:
+            print('Could not fetch interface for machine : "{}"'.format(e))
+            return 'FAILURE'
+
+        for i in interfaces:
+            if str(i['identifier']) == self.interface:
+                return i['mac']
+
+    def get_netmask(self):
+        try:
+            interfaces = self.get_interfaces()
+        except ProvisionerError as e:
+            print('Could not fetch interface for machine : "{}"'.format(e))
+            return 'FAILURE'
+
+        for i in interfaces:
+            if str(i['identifier']) == self.interface:
+                return i['netmaskv4']
---
+++ mrp_client.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python3
+
+from library.get_ip import IPGetter
+import argparse
+
+class Client(object):
+    def __init__(self, mrp_token, mrp_url):
+        self.token = mrp_token
+        self.url = mrp_url
+
+    def getip(self, machine_name, interface_name='eth1'):
+        ipgetter = IPGetter(self.url, self.token, machine_name, interface_name)
+        return ipgetter.get_ip()
+
+    def getmac(self, machine_name, interface_name='eth1'):
+        ipgetter = IPGetter(self.url, self.token, machine_name, interface_name)
+        return ipgetter.get_mac()
+
+    def getnetmask(self, machine_name, interface_name='eth1'):
+        ipgetter = IPGetter(self.url, self.token, machine_name, interface_name)
+        return ipgetter.get_netmask()
+
+if __name__ == '__main__':
+    """This is the point of entry of our application, not much logic here"""
+    parser = argparse.ArgumentParser(description='Run some benchmark.')
+    parser.add_argument('action', type=str, default='',
+                        help='getip, getmac, getnetmask')
+    parser.add_argument('machine_name', type=str, default='',
+                        help='name of the machine')
+    parser.add_argument('--mrp-url', type=str, default='',
+                        help='The URL of the MrP server')
+    parser.add_argument('--mrp-token', type=str,
+                        help='The authentication token to use')
+    parser.add_argument('-v', '--verbose', action='count', default=0,
+                        help='The verbosity of logging output')
+    args = parser.parse_args()
+
+    client = Client(args.mrp_token, args.mrp_url)
+    if args.action == 'getip':
+        print(client.getip(args.machine_name))
+    elif args.action == 'getmac':
+        print(client.getmac(args.machine_name))
+    elif args.action == 'getnetmask':
+        print(client.getnetmask(args.machine_name))
+    else:
+        parser.print_help()
